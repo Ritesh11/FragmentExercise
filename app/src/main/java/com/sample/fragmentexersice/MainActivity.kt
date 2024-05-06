d6ecabc311e47c2eeda2fad95fa33fa5dc52faf6
Girmiti <riteshksingh76@gmail.com>
2024-05-06 13:50:46 +0530

    Here we are using field injection, instead of infecting a object one by one we can inject the MainActivity in Component class. and from Main activity we are simply do @inject on any Object to get its instance.

--- app/src/main/java/com/sample/fragmentexersice/MainActivity.kt
+++ app/src/main/java/com/sample/fragmentexersice/MainActivity.kt
@@ -2,20 +2,29 @@ package com.sample.fragmentexersice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sample.daggerexample.SmartPhone
 import com.sample.fragmentexersice.dagger.component.DaggerSmartPhoneComponent
 import com.sample.fragmentexersice.dagger.component.SmartPhoneComponent
 import com.sample.fragmentexersice.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        DaggerSmartPhoneComponent.create().getSmartPhone().makeACallWithRecording()
+//        DaggerSmartPhoneComponent.create().getSmartPhone().makeACallWithRecording()
+
+
+        DaggerSmartPhoneComponent.create().inject(this)
+        smartPhone.makeACallWithRecording()
 
     }
 }
--- app/src/main/java/com/sample/fragmentexersice/dagger/component/SmartPhoneComponent.kt
+++ app/src/main/java/com/sample/fragmentexersice/dagger/component/SmartPhoneComponent.kt
@@ -1,6 +1,7 @@
 package com.sample.fragmentexersice.dagger.component
 
 import com.sample.daggerexample.SmartPhone
+import com.sample.fragmentexersice.MainActivity
 import com.sample.fragmentexersice.dagger.module.BatteryModule
 import com.sample.fragmentexersice.dagger.module.MemoryCardModule
 import dagger.Component
@@ -8,4 +9,6 @@ import dagger.Component
 @Component(modules = [MemoryCardModule::class, BatteryModule::class])
 interface SmartPhoneComponent {
     fun getSmartPhone(): SmartPhone
+
+    fun inject(mainActivity: MainActivity)
 }
